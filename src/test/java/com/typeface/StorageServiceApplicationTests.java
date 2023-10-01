ae25249ce081472cff0478862c53694f13198c80
raghuchandra <rkanakamedal@microsoft.com>
2023-10-01 15:08:26 +0530

    all changes made

--- src/test/java/com/typeface/StorageServiceApplicationTests.java
+++ src/test/java/com/typeface/StorageServiceApplicationTests.java
@@ -1,13 +1,42 @@
-package com.javatechie;
+package com.typeface;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class StorageServiceApplicationTests {
+public class StorageServiceApplicationTests {
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	MockMvc mockMvc;
 
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 	@Test
-	void contextLoads() {
+	public void uploadTest() throws Exception{
+		MockMultipartFile file
+				= new MockMultipartFile(
+				"file",
+				"hello.txt",
+				MediaType.TEXT_PLAIN_VALUE,
+				"Hello, World!".getBytes()
+		);
+
+		MockMultipartHttpServletRequestBuilder multipartRequest =
+				MockMvcRequestBuilders.multipart("/files/upload");
+		mockMvc.perform(multipartRequest.file(file))
+				.andExpect(status().isOk());
 	}
 
 }
